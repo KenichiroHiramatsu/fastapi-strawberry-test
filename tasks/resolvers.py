20e2a448c068066e112022e91414b099870b27f4
matsukenbass <matsukenbass@gmail.com>
2022-05-15 23:56:01 +0900

    requirements.txtを修正

--- tasks/resolvers.py
+++ tasks/resolvers.py
@@ -6,6 +6,14 @@ from tasks.repositories import InMemoryRepository
 from tasks.services import TaskService
 from tasks.types import TaskType
 
+import pymongo
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+CONNECTION_URL = os.getenv('CONNECTION_URL')
+
 
 def get_task(id: strawberry.ID) -> TaskType:
     db = InMemoryRepository
@@ -16,6 +24,13 @@ def get_task(id: strawberry.ID) -> TaskType:
 
 
 def get_tasks() -> List[TaskType]:
+    # client = pymongo.MongoClient(CONNECTION_URL)
+    # db = client.FastApiDB.tasks
+    # print(db)
+    # document = db.find()
+    # client.close()
+    # response = [document[i] for i in range(document.count())]
+
     db = InMemoryRepository
     service = TaskService(db)
     tasks = service.find_all()
