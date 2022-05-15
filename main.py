50f670d938b66d50452a738fcad813bf3483e290
matsukenbass <matsukenbass@gmail.com>
2022-05-15 15:47:27 +0900

    元に戻した

--- main.py
+++ main.py
@@ -38,46 +38,46 @@ app.include_router(graphql_app, prefix="/graphql")
 
 @app.get("/")
 def index():
-    # return {"Hello": "World from FastAPI"}
+    return {"Hello": "World from FastAPI"}
 
-    page = st.sidebar.selectbox("Choose your page", ['taskList', 'addTask'])
+    # page = st.sidebar.selectbox("Choose your page", ['taskList', 'addTask'])
 
-    if page == 'addTask':
-        st.title("APIテスト(addTask)")
-        with st.form(key='add'):
-            title: str = st.text_input("タスク名", max_chars=12)
-            description: Optional[str] = st.text_input("説明")
-            data = {
-                'title': title,
-                'description': description
-            }
-            submit_button = st.form_submit_button(label="送信")
+    # if page == 'addTask':
+    #     st.title("APIテスト(addTask)")
+    #     with st.form(key='add'):
+    #         title: str = st.text_input("タスク名", max_chars=12)
+    #         description: Optional[str] = st.text_input("説明")
+    #         data = {
+    #             'title': title,
+    #             'description': description
+    #         }
+    #         submit_button = st.form_submit_button(label="送信")
 
-        if submit_button:
-            st.write("## 送信データ")
-            st.json(data)
-            st.write("## レスポンス結果")
-            url = 'http://127.0.0.1:8000/graphql'
-            headers = {
-                "Content-Type": "application/json",
-            }
-        # graphqlのqueryを記述
-            query = """
-            mutation($input:AddTaskInput!) {
-            taskAdd(taskInput:$input) {
-                id
-                title
-                description
-            }
-            }
-            """
-            variables = {
-                "input": data
-            }
-            response = requests.post(
-                url, json={"query": query, "variables": variables}, headers=headers
-            )
-            st.json(response.content.decode())
+    #     if submit_button:
+    #         st.write("## 送信データ")
+    #         st.json(data)
+    #         st.write("## レスポンス結果")
+    #         url = 'http://127.0.0.1:8000/graphql'
+    #         headers = {
+    #             "Content-Type": "application/json",
+    #         }
+    #     # graphqlのqueryを記述
+    #         query = """
+    #         mutation($input:AddTaskInput!) {
+    #         taskAdd(taskInput:$input) {
+    #             id
+    #             title
+    #             description
+    #         }
+    #         }
+    #         """
+    #         variables = {
+    #             "input": data
+    #         }
+    #         response = requests.post(
+    #             url, json={"query": query, "variables": variables}, headers=headers
+    #         )
+    #         st.json(response.content.decode())
 
     elif page == 'taskList':
         st.title("工事中")
